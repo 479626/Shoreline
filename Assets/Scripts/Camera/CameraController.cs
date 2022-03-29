b2287a2b9bce2f8a91b8f0cfded24781c21b128c
KimiNoUso <90899255+479626@users.noreply.github.com>
2022-03-29 18:41:16 +0100

    Changed Camera & Scene classes

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Position")]
+    public Transform player;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    void Start()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position != player.position)
+        {
+            Vector3 playerPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            playerPosition.x = Mathf.Clamp(playerPosition.x, minPosition.x, maxPosition.x);
+            playerPosition.y = Mathf.Clamp(playerPosition.y, minPosition.y, maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position, playerPosition, smoothing);
+
+        }
+    }
+
+    private Vector3 RoundPosition(Vector3 position)
+    {
+        float xOffset = position.x % .0625f;
+        if (xOffset != 0)
+        {
+            position.x -= xOffset;
+        }
+        float yOffset = position.y % .0625f;
+        if (yOffset != 0)
+        {
+            position.y -= yOffset;
+        }
+        return position;
+    }
+}
---
+++ Assets/Scripts/Camera/SceneTransition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public int scene;
+    public Vector2 playerPos;
+    public VectorValue playerStorage;
+    public Interaction interaction;
+
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player") && !col.isTrigger)
+        {
+            interaction.InteractOn();
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                playerStorage.initialValue = playerPos;
+                SceneManager.LoadScene(scene);
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player") && !col.isTrigger)
+        {
+            interaction.InteractOff();
+        }
+    }
+}
