c56924b4b19f247474ee64b643e5a782b8af6fe6
KimiNoUso <90899255+479626@users.noreply.github.com>
2022-03-31 16:33:49 +0100

    Fixed blacksmith door trigger

---
+++ Assets/Scripts/Level1/LevelOneBlackmith.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneBlackmith : MonoBehaviour
+{
+    public Interaction interaction;
+    bool triggerDialogue;
+
+    void Start()
+    {
+        triggerDialogue = false;
+    }
+
+    void Update()
+    {
+        CheckForDialogue();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            interaction.InteractOn();
+            triggerDialogue = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            interaction.InteractOff();
+        }
+    }
+
+    void CheckForDialogue()
+    {
+        if (triggerDialogue == true)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                DialogueInteraction trigger = gameObject.GetComponent<DialogueInteraction>();
+                trigger.TriggerDialogue();
+                triggerDialogue = false;
+                interaction.InteractOff();
+            }
+        }
+    }
+
+}
