86ef8137ece80ef05fac975a2da8c533a1b55c7b
KimiNoUso <90899255+479626@users.noreply.github.com>
2022-04-01 22:37:38 +0100

    Add combat system

--- Assets/Scripts/Level2/LevelTwoPlayer.cs
+++ Assets/Scripts/Level2/LevelTwoPlayer.cs
@@ -4,25 +4,78 @@ using UnityEngine;
 
 public class LevelTwoPlayer : MonoBehaviour
 {
-    public float speed = 3.5f;
+    private static GameObject player;
     public Rigidbody2D rb;
     public Animator animator;
-    private static GameObject player;
+    public Transform range;
+    public LayerMask enemyLayer;
+    public HealthBar healthBar;
+    public GameObject warrior;
     Vector2 movement;
+
+    public float attackRange = 0.5f;
+    public float speed = 3.5f;
+    public int maxHealth, currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetButtonDown("Fire1"))
+            Attack();
+        else MoveLogic();
+    }
+
+    void MoveLogic()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    void Attack()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        int damage = Random.Range(8, 10);
+        StartCoroutine(Attacking());
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(range.position, attackRange, enemyLayer);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            warrior.GetComponent<LevelTwoWarrior>().TakeDamage(damage);
+        }
+    }
+
+    IEnumerator Attacking()
+    {
+        speed = 0f;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(0.5f);
+        speed = 3.5f;
+        yield break;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (range == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(range.position, attackRange);
     }
 
 }
