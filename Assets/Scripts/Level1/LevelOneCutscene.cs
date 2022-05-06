290db80ce723f4edbfc66ac1641fdbdc911e06c6
Joshua Bulman <479626@chrome.escg.ac.uk>
2022-05-06 14:10:48 +0100

    Add door to cutscene script

---
+++ Assets/Scripts/Level1/LevelOneCutscene.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelOneCutscene : MonoBehaviour
+{
+    public int scene, cutscene;
+    public Animator anim;
+    public PlayerStats stats;
+    public Vector2 playerPos;
+    public VectorValue playerStorage;
+    public Interaction interaction;
+
+    private void MovePlayer()
+    {
+        if (!stats.seenLevelOneCutscene)
+        {
+            stats.seenLevelOneCutscene = true;
+            SoundManager.instance.DoorSound();
+            playerStorage.initialValue = playerPos;
+            SceneManager.LoadScene(cutscene);
+        }
+        else
+        {
+            SoundManager.instance.DoorSound();
+            playerStorage.initialValue = playerPos;
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+    #region Collision Detection
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (!col.CompareTag("Player") || col.isTrigger) return;
+
+        interaction.InteractOn();
+        if (SceneManager.GetActiveScene().buildIndex == 2 || SceneManager.GetActiveScene().buildIndex == 8)
+        {
+            anim.SetBool("Open", true);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            MovePlayer();
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player") && !col.isTrigger)
+        {
+            anim.SetBool("Open", false);
+            interaction.InteractOff();
+        }
+    }
+
+    #endregion
+}
